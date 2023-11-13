10818b2930cdbe636094032185d201c84c4a1627
Ridwanul Kabir <ridwanulkabir12995@gmail.com>
2023-11-13 15:34:27 +0600

    44-3 Create Nav toggle and navbar animation

--- src/Navber/Navber.jsx
+++ src/Navber/Navber.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from '../Link/Link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faBars,  faXmark} from '@fortawesome/free-solid-svg-icons';
+
 
 const Navber = () => {
-    const routes = [  { id: 1, name: 'Home', path: '/', exact: true },
+    const routes = [  { id: 1, name: 'Home', path: '/', },
         { id: 2, name: 'About', path: '/about' },
         { id: 3, name: 'Contact', path: '/contact' },
         { id: 4, name: 'Products', path: '/products' },
         { id: 5, name: 'Not Found', path: '*' },
       ];
-      
-    
+      const [open, setopen] =useState(false)
+    // font awesome icone add 
     return (
-        <nav>
+        <nav> <div class="m-4" onClick={()=> setopen(!open)}> {open===true? 
+            <FontAwesomeIcon icon={faXmark} > </FontAwesomeIcon>
+            :  <FontAwesomeIcon icon={faBars} ></FontAwesomeIcon>}
+    
+    </div>
             <ul class="lg:flex m-4">
             {
                 routes.map(route=> <Link route={route}></Link>)
